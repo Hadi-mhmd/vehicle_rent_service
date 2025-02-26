0e425f754245771e736207e62c1efc21bb4e02e0
Hadi-mhmd <hadiy.mohamadd@gmail.com>
2025-02-27 00:07:44 +0200

    vehicle class generated

---
+++ bob.py
@@ -0,0 +1,12 @@
+class Vehicle:
+    def __init__(self,brand,year,rental_price_per_day):
+        self.brand = brand
+        self.year = year
+        self.rental_price_per_day = rental_price_per_day
+    
+    def display_info(self):
+        print(f"{self.brand} {self.model}, Year: {self.year}, Rental price: ${self.rental_price_per_day}/day")
+
+    def calculate_rental_cost(self,days):
+        return  self.rental_price_per_day * days   
+
