60e179990b7b119ff131df4f7633dc5e0584d7c6
Hadi-mhmd <hadiy.mohamadd@gmail.com>
2025-02-27 23:51:24 +0200

    car and motorcycle class created , display info method overriden

--- bob.py
+++ bob.py
@@ -10,3 +10,20 @@ class Vehicle:
     def calculate_rental_cost(self,days):
         return  self.rental_price_per_day * days   
 
+
+class Car(Vehicle):
+    def __init__(self,brand,year,rental_price_per_day,seating_capacity):
+        self.seating_capacity = seating_capacity
+        super().__init__(self,brand,year,rental_price_per_day)
+
+    def display_info(self):
+        print(f"{self.brand} {self.model}, Year: {self.year},Seats number:{self.seating_capacity} Rental price: ${self.rental_price_per_day}/day")
+
+
+class Motorcycle(Vehicle):
+     def __init__(self,brand,year,rental_price_per_day,engine_capacity):
+        self.engine_capacity = engine_capacity
+        super().__init__(self,brand,year,rental_price_per_day) 
+
+    def display_info(self):
+        print(f"{self.brand} {self.model}, Year: {self.year}, Engine power: {self.engine_capacity} Rental price: ${self.rental_price_per_day}/day")      
